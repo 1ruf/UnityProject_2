163afb7910e5df2121bcba2d32f387e650427d24
sotodrkrgo <sotodrkrgo44@gmail.com>
2024-10-28 21:36:44 +0900

    Docs fix
    a

--- Assets/2.Scripts/KHJ/Skill/CharacterChanged.cs
+++ Assets/2.Scripts/KHJ/Skill/CharacterChanged.cs
@@ -9,9 +9,10 @@ public class CharacterChanged : MonoBehaviour
 
     public void CharacterChange(Player agent, KSB_Enemy enemy)
     {
-        agent.GetComponent<SpriteRenderer>().sprite = enemy.GetComponent<SpriteRenderer>().sprite;
+        agent.GetComponent<SpriteRenderer>().sprite = enemy.Visual_Sprite;
         agent.transform.position = enemy.transform.position;
         agent.RbCompo.velocity = Vector2.zero;
+        enemy.gameObject.SetActive(false);
         print("º¯°æ");
     }
 }
