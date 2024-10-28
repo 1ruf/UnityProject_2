03822f69eff728ff7fcfd68cecefb519e74d17e5
sotodrkrgo <sotodrkrgo44@gmail.com>
2024-10-28 13:10:04 +0900

    CharChange
    script Fix

--- Assets/2.Scripts/KHJ/State/ChrChangeState.cs
+++ Assets/2.Scripts/KHJ/State/ChrChangeState.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class ChrChangeState : State
 {
-
+    [SerializeField] private CharacterChanged _characterChanged;
     protected override void EnterState()
     {
         RaycastHit2D ray = Physics2D.Raycast(_agent.InputCompo.MousePos, new Vector3(0,0,-10));
         if (!ray) return;
+
+        _characterChanged.CharacterChange(_agent, ray.transform.GetComponent<Enemy>());
         
     }
 }
