3915f4be34cba2450c1e9693fa7db56ea68ce42d
1_ruf <gyeol0411@naver.com>
2024-11-07 15:41:10 +0900

    audio
    ---------------------------------------

---
+++ Assets/2.Scripts/KHG/Menu/MenuBGMManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuBGMManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip _audioClip;
+
+    public AudioSource _audio { get; set; }
+    [Header("setting")]
+    [SerializeField] private bool playOnEnabled;
+    private void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+    private void OnEnable()
+    {
+        _audio.clip = _audioClip;
+        _audio.pitch = 0;
+        if (playOnEnabled == true)
+        {
+            PlayBGMWithPitch(_audio,0.9f, 1.5f, true);
+        }
+    }
+    public void SetBGM(AudioSource audio,bool play)
+    {
+        if (play == true)
+        {
+            audio.Play();
+        }
+        else
+        {
+            audio.Pause();
+        }
+        
+    }
+    public void PlayBGMWithPitch(AudioSource audio,float pitch,float time,bool play)
+    {
+        if (play == true)
+        {
+            audio.DOPitch(pitch, time);
+            audio.Play();
+        }
+        else
+        {
+            audio.DOPitch(0, time);
+            audio.Play();
+        }
+    }
+}
