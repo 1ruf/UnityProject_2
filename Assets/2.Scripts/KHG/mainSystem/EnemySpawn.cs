2d31c7ee0bce730e4ce007a0feeff35c90347b9e
1_ruf <gyeol0411@naver.com>
2024-11-26 23:47:21 +0900

    f

--- Assets/2.Scripts/KHG/mainSystem/EnemySpawn.cs
+++ Assets/2.Scripts/KHG/mainSystem/EnemySpawn.cs
@@ -10,20 +10,22 @@ public class EnemySpawn : MonoBehaviour
 
     [SerializeField] private Transform _enemyParent;
 
+    public List<GameObject> cloneEnemys;
     public void SpawnEnemy()
     {
         foreach (GameObject enemy in _enemy)
         {
-            float SP_X = Random.Range(-_maxSpawnPoint.x,_maxSpawnPoint.x);
-            float SP_Y = Random.Range(-_maxSpawnPoint.y,_maxSpawnPoint.y);
+            float SP_X = Random.Range(-_maxSpawnPoint.x/2,_maxSpawnPoint.x/2);
+            float SP_Y = Random.Range(-_maxSpawnPoint.y/2,_maxSpawnPoint.y/2);
             Vector3 SP = new Vector3(SP_X, SP_Y);
             Spawn(transform.position + SP, enemy);
         }
     }   
     private void Spawn(Vector2 spawnpoint, GameObject target)
     {
-        GameObject clone = Instantiate(target, _enemyParent);
+        GameObject clone = Instantiate(target, null);
         clone.transform.position = spawnpoint;
+        cloneEnemys.Add(clone);
     }
     private void OnDrawGizmos()
     {
