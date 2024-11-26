fc03d53cf9dfc9f83347ae10fb2728ea9686972e
1_ruf <gyeol0411@naver.com>
2024-11-26 13:07:32 +0900

    skilladded

--- Assets/2.Scripts/KHJ/01. Script/PlayerUI/SkillUI.cs
+++ Assets/2.Scripts/KHJ/01. Script/PlayerUI/SkillUI.cs
@@ -1,49 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SkillUI : MonoBehaviour
 {
-    [SerializeField] private float _coolTime = 5f;
-
-    public bool CanSkill { get; private set; } = true;
-    private Image _image;
-
-    private Coroutine _coolCoroutine;
+    private SpriteRenderer _renderer;
 
     private void Awake()
     {
-        _image = GetComponent<Image>();
+        _renderer = GetComponent<SpriteRenderer>();
     }
 
-    private void Start()
+    public void VisibleChange(bool value)
     {
-        
+        Color clr = _renderer.color;
+        _renderer.color = value ? Visible(clr) : Invisible(clr);
     }
 
-    public void Cool()
+    public void SetAlpha(int value) //max = 255
     {
-        _coolCoroutine = StartCoroutine(SkillCool());
+        if (value > 255) return;
+        Color clr = _renderer.color;
+        SetVisible(clr, value);
     }
 
 
-    public void CoolTimeReSet() // 해킹 스킬 사용하면 호출 ( 고유스킬의 쿨타임을 초기화하여 해킹 후 바로 스킬 사용 가능
+
+
+
+
+
+    //-----------------------------------------------------------------------------
+    /// <summary>
+    /// clr라는 이름은 color 의 약자, clr.r,g,b를 받아와서 그대로 적용후
+    /// alpha값만 0 또는 255로 적용
+    /// </summary>
+    /// <param name="clr"></param>
+    /// <returns></returns>
+    private Color Invisible(Color clr)
     {
-        if (_coolCoroutine != null) StopCoroutine(_coolCoroutine);
-        CanSkill = true;
-        _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, 1f);
+        return new Color(clr.r, clr.g, clr.b, 0);
     }
-
-
-    private IEnumerator SkillCool() // 스킬 UI 꺼진 것 처럼 보이게함, 스킬 사용 불가능 상태로 만들고 쿨타임 후에 원상복구
+    private Color Visible(Color clr)
     {
-        CanSkill = false;
-        _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, 0.5f); 
-        yield return new WaitForSeconds(_coolTime);
-        CanSkill = true;
-        _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, 1f);
+        return new Color(clr.r, clr.g, clr.b, 255);
     }
-
-
+    private Color SetVisible(Color clr,int alpha)
+    {
+        return new Color(clr.r, clr.g, clr.b, alpha);
+    }
+    //-----------------------------------------------------------------------------
 }
