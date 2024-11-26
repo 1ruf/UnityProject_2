cbc16ef7184cbc32ca82213b1d15f74976f8e9ae
sotodrkrgo <sotodrkrgo44@gmail.com>
2024-11-26 23:44:58 +0900

    fix : Skill

--- Assets/2.Scripts/KHJ/Skills.cs
+++ Assets/2.Scripts/KHJ/Skills.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,14 @@ public class Skills : MonoBehaviour
     static public Skills Instance;
 
     float _attackCool = 11;
-    private bool _canAttack = true;
+    public bool _canAttack = true;
 
 
     private Vector2 _size;
     private Color _color;
     private float _animSpeed;
     private float _damage;
+    private float _speed;
 
     private void Awake()
     {
@@ -27,21 +29,23 @@ public class Skills : MonoBehaviour
 
     public void UseOrcSkill(Entity Orc)
     {
+        if (_canAttack == false) return;
         _size = Orc.transform.localScale;
 
-
+        _speed = Orc.Data.moveSpeed;
         _size = Orc.transform.localScale;
         _damage = Orc.Data.Damage;
         _animSpeed = 1;
         _color = Orc.GetComponent<SpriteRenderer>().color;
 
-
+        Orc.SetMoveSpeed(_speed);
         Orc.transform.localScale = _size * 1.2f;
         Orc.Data.Damage = _damage * 1.5f;
-        Orc.AnimCompo.Animator.speed = 3;
+        Orc.AnimCompo.Animator.speed = 2.5f;
         Orc.GetComponent<SpriteRenderer>().color = Color.red;
 
         StartCoroutine(EndSkill(Orc));
+        StartCoroutine(AttackCoolTime());
     }
 
     IEnumerator EndSkill(Entity Orc)
@@ -51,7 +55,13 @@ public class Skills : MonoBehaviour
         Orc.Data.Damage = _damage;
         Orc.AnimCompo.Animator.speed = _animSpeed;
         Orc.GetComponent<SpriteRenderer>().color = _color;
-        StartCoroutine(AttackCoolTime());
+    }
+
+    IEnumerator EndSkill2(Entity Orc)
+    {
+        yield return new WaitForSeconds(duration);
+        Orc.SetMoveSpeed(_speed);
+        Orc.transform.localScale = _size;
     }
 
     IEnumerator AttackCoolTime()
@@ -61,5 +71,25 @@ public class Skills : MonoBehaviour
         _canAttack = true;
     }
 
+    internal void ReSetCoolTime()
+    {
+        StopCoroutine(AttackCoolTime());
+        _canAttack = true;
+    }
 
+
+    public void UseLSkill(Entity Orc)
+    {
+        if (_canAttack == false) return;
+        _size = Orc.transform.localScale;
+
+        _speed = Orc.Data.moveSpeed;
+
+
+        Orc.SetMoveSpeed(_speed +5);
+        Orc.transform.localScale = _size * 0.8f;
+
+        StartCoroutine(EndSkill2(Orc));
+        StartCoroutine(AttackCoolTime());
+    }
 }
